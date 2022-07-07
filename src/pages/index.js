662b54f6a1d32b3a29947d9c0e34f19aafda08e8
jesse h <jesselevihall@gmail.com>
2022-07-06 18:44:40 -0600

    sections complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
 
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from '../sections/banner';
-import KeyFeature from '../sections/key-feature';
-import ServiceSection from '../sections/service-section';
-import Feature from '../sections/feature';
-import CoreFeature from '../sections/core-feature';
-import WorkFlow from '../sections/workflow';
-import Package from '../sections/package';
-import TeamSection from '../sections/team-section';
-import TestimonialCard from '../sections/testimonial';
+import SEO from "components/seo";
+import Layout from "components/layout";
+import Banner from "../sections/banner";
+import KeyFeature from "../sections/key-feature";
+import ServiceSection from "../sections/service-section";
+import Feature from "../sections/feature";
+import CoreFeature from "../sections/core-feature";
+import WorkFlow from "../sections/workflow";
+import Package from "../sections/package";
+import TeamSection from "../sections/team-section";
+import TestimonialCard from "../sections/testimonial";
+import BlogSection from "../sections/blog-section";
+import Subscribe from "../sections/subscribe";
 
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
-        <Layout>
-          <SEO title="NextJS Landing Page" />
-          <Banner />
-          <KeyFeature />
-          <ServiceSection />
-          <Feature />
-          <CoreFeature />
-          <WorkFlow />
-          <Package />
-          <TeamSection />
-          <TestimonialCard />
-        </Layout>
+      <Layout>
+        <SEO title="NextJS Landing Page" />
+        <Banner />
+        <KeyFeature />
+        <ServiceSection />
+        <Feature />
+        <CoreFeature />
+        <WorkFlow />
+        <Package />
+        <TeamSection />
+        <TestimonialCard />
+        <BlogSection />
+        <Subscribe />
+      </Layout>
     </ThemeProvider>
   );
 }
