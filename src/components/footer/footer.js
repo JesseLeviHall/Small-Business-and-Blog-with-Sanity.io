9b064228a9fc353d0669dfe0a43720037178352e
jesse h <jesselevihall@gmail.com>
2022-07-07 19:22:29 -0600

    logo change

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@
 import { jsx, Box, Container, Image, Text } from "theme-ui";
 import { Link } from "components/link";
 import data from "./footer.data";
-import FooterLogo from "assets/logo.svg";
+import FooterLogo from "assets/logo.png";
 
 export default function Footer() {
   return (
@@ -10,7 +10,7 @@ export default function Footer() {
       <Container>
         <Box sx={styles.footer.footerBottomArea}>
           <Link path={"/"}>
-            <Image src={FooterLogo} alt="Logo" />
+            <Image sx={{ width: "250px" }} src={FooterLogo} alt="Logo" />
           </Link>
           <Box sx={styles.footer.menus}>
             <nav>
